73ea94611016531f3e5b0fe9046923cc58dd200e
Oktandrew <158817138+Oktandrew@users.noreply.github.com>
2025-08-10 02:23:40 +0300

    Update color_handler_night_light.h

--- components/xiaomi_bslamp2/light/color_handler_night_light.h
+++ components/xiaomi_bslamp2/light/color_handler_night_light.h
@@ -38,7 +38,7 @@ class ColorHandlerNightLight : public ColorHandler {
     // matches the night light of the original firmware.
     if (v.get_color_mode() == light::ColorMode::COLOR_TEMPERATURE) {
       red = 0.963f; //0.968
-      green = 0.963f; //0.968
+      green = 0.965f; //0.968
       blue = 0.968f; //0.972
       white = 0.0f;
     }
@@ -46,10 +46,10 @@ class ColorHandlerNightLight : public ColorHandler {
     // specific color, instead of the default. This is a nice extra for
     // this firmware, as the original firmware does not support it.
     else {
-      red = std::lerp(0.9997f, 0.960f, v.get_red()); // 0.9997f, 0.9680f
-      green = std::lerp(0.9997f, 0.960f, v.get_green()); // 0.9997f, 0.9680f
+      red = std::lerp(0.9997f, 0.963f, v.get_red()); // 0.9997f, 0.9680f
+      green = std::lerp(0.9997f, 0.965f, v.get_green()); // 0.9997f, 0.9680f
       auto blue_scale = (v.get_red() + v.get_green()) / 2.0f;
-      auto blue_max = std::lerp(0.960f, 0.960f, blue_scale); //0.9640f, 0.9720f
+      auto blue_max = std::lerp(0.960f, 0.968f, blue_scale); //0.9640f, 0.9720f
       blue = std::lerp(0.9997f, blue_max, v.get_blue()); //0.9997f
       white = 0.0f;
     }
