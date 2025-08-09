8a0c036de7892f3ecbecf991aa2768527574932d
Oktandrew <158817138+Oktandrew@users.noreply.github.com>
2025-08-10 02:13:13 +0300

    Update color_handler_night_light.h

--- components/xiaomi_bslamp2/light/color_handler_night_light.h
+++ components/xiaomi_bslamp2/light/color_handler_night_light.h
@@ -37,7 +37,7 @@ class ColorHandlerNightLight : public ColorHandler {
     // Based on measurements using the original device firmware, so it
     // matches the night light of the original firmware.
     if (v.get_color_mode() == light::ColorMode::COLOR_TEMPERATURE) {
-      red = 0.965f; //0.968
+      red = 0.963f; //0.968
       green = 0.963f; //0.968
       blue = 0.965f; //0.972
       white = 0.0f;
